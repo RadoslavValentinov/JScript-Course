8d2d0ec0ea0c87b9773297679864a13425b14412
Radoslav Hristov <160596999+RadoslavValentinov@users.noreply.github.com>
2024-03-03 14:33:49 +0200

    Add files via upload

---
+++ JS Fundamental/List Of Names/list.js
@@ -0,0 +1,9 @@
+
+    function sortNames(names) {
+        names.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+        for (const [index, name] of names.entries()) {
+            console.log(`${index + 1}.${name}`);
+        }
+    }
+
+sortNames(["John", "Bob", "", "Ema"]);
